45520438422e6e05117af44043525b97e4c4799f
Pablo <74419051+MrChova20@users.noreply.github.com>
2022-10-31 21:58:45 +0100

    Primera Implementación FrontEnd Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+
 import { HashRouter as Router } from 'react-router-dom';
 import { Header } from './components';
 import BaseRouter from './router/router';
--- frontend/src/components/test.component.js
+++ frontend/src/components/test.component.js
@@ -1,26 +1,45 @@
 import React from "react";
+
+
+
 class Test extends React.Component{
 
-
-    constructor(props){
-        super(props);
-   
-    }
 
 
 
 
     render(){
         return(
-            <div>
-                Hola esto es el componente test
+
+              
+             <div className="screen-1">
+                <div className="screen-2">
+                 <h3> Iniciar Sesión </h3>
+               
+          <><div className="email">
+                    <label htmlFor="email">Email</label>
+                    <div className="sec-2">
+                        <ion-icon name="mail-outline" />
+                        <input type="email" name="email" placeholder="usuario@example.com" />
+                    </div>
+                </div><div className="password">
+                    <label htmlFor="password">Contraseña</label>
+                    <div className="sec-2">
+                        <ion-icon name="lock-closed-outline" />
+                        <input className="pas" type="password" name="password" placeholder="············" />
+                        <ion-icon className="show-hide" name="eye-outline" />
+                    </div>
+                </div><button className="login">Entrar </button><div className="footer"><span>                   </span><span>   </span></div></>
+        </div>
+       
+         </div>
+
              
-
-
-                </div>
         )
     }
 }
+
+
 
 
 export default Test
