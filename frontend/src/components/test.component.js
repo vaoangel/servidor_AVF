832729620bc6d3dd2ecff553a1ba7a7b9ff226c3
Pablo <74419051+MrChova20@users.noreply.github.com>
2022-11-01 13:53:50 +0100

    Login App Frontend

--- frontend/src/components/test.component.js
+++ frontend/src/components/test.component.js
@@ -1,32 +1,40 @@
 import React from "react";
-
+import logo from '../assets/img/logo.png'
+import fotouser  from '../assets/img/usuario.png'
+import contra  from '../assets/img/contra.png'
 
 
 class Test extends React.Component{
 
-
+   
 
 
 
     render(){
         return(
 
-              
-             <div className="screen-1">
+               
+             <div className="screen-1"> 
+             <img src={logo} alt="image not found"className="logoFoto" />
                 <div className="screen-2">
-                 <h3> Iniciar Sesión </h3>
+                 <h3>Entrar</h3>
+                
+
+   
                
           <><div className="email">
-                    <label htmlFor="email">Email</label>
+                    
+                    <img src={fotouser} alt="image not found"className="userFoto" />
                     <div className="sec-2">
                         <ion-icon name="mail-outline" />
-                        <input type="email" name="email" placeholder="usuario@example.com" />
+                        <input type="email" name="email" placeholder="Usuario" />
                     </div>
                 </div><div className="password">
-                    <label htmlFor="password">Contraseña</label>
+                <img src={contra} alt="image not found"className="contraFoto" />
+                  
                     <div className="sec-2">
                         <ion-icon name="lock-closed-outline" />
-                        <input className="pas" type="password" name="password" placeholder="············" />
+                        <input className="pas" type="password" name="password" placeholder="Contraseña" />
                         <ion-icon className="show-hide" name="eye-outline" />
                     </div>
                 </div><button className="login">Entrar </button><div className="footer"><span>                   </span><span>   </span></div></>
