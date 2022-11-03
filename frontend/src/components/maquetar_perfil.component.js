4404f2bea032751eb48e99817aba838feb2bcc91
Ivan <ivandiscobolo@gmail.com>
2022-11-03 12:00:23 +0100

    Maquetación pagina editar perfil

--- frontend/src/components/maquetar_perfil.component.js
+++ frontend/src/components/maquetar_perfil.component.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {  Link } from 'react-router-dom';
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
 class MaquetarPerfil extends React.Component{
 
 
@@ -12,12 +14,36 @@ class MaquetarPerfil extends React.Component{
 
     render(){
         return(
-            <div>
-                Hola esto es el componente MaquetarPerfil
-             
+            <div className="screen-3">
+                <h4 className="h4">Datos del perfil</h4>  
 
+                <Form className="form">
+                    <div className="label">
+                        <label className="label-text">Nombre</label>
+                    </div>
+                    <div className="form-input">
+                        <input type="text" name="name" placeholder="Ivan Villanueva"/>
+                    </div>
+                    <div className="label">
+                        <label className="label-text">Correo</label>
+                    </div>
+                    <div className="form-input">
+                        <input type="text" name="mail" placeholder="fwfwe@gmail.com"/>
+                    </div>
+                    <div className="label">
+                        <label className="label-text">Número telefónico</label>
+                    </div>
+                    <div className="form-input">
+                        <input type="text" name="phone" placeholder="4242432442"/>
+                    </div>
+                    <div>
+                        <button type="button" className="button">Guardar</button>
+                    </div>
+                    
+                </Form>
 
-                </div>
+            </div>
+                
         )
     }
 }
