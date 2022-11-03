fbb1892cf538cc2ca1c36aa5b2081c300d258e89
vaoangel <33663543+vaoangel@users.noreply.github.com>
2022-11-03 09:58:03 +0100

    tests

--- backend/test.js
+++ backend/test.js
@@ -2,11 +2,13 @@
 // mainTest1.js
 // ........................................................
 const arduino = require('./controllers/arduino.controller')
+const login = require("./handlers/login.handler")
+const user = require("./handlers/user.handler")
 var assert = require("assert")	//El mocha necesita esta clase para hacer las pruebas
 // ........................................................
 // main ()
 // ........................................................
-describe("Test 1: insertar una Medicion", function () {	//las pruebas se hacen dentro de esta función siempre
+describe("Test 1: Login de usuario", function () {	//las pruebas se hacen dentro de esta función siempre
 	// ....................................................
 	// ....................................................
 
@@ -17,23 +19,21 @@ describe("Test 1: insertar una Medicion", function () {	//las pruebas se hacen d
 	// ....................................................
 
 	//PRUEBA ESCRITA SIN TRY CATCH
-	it("puedo insertar y buscar una medicion",	//Esta prueba es importante porque usa assert
+	it("puedo loguearme con un usuario y contraseña",	//Esta prueba es importante porque usa assert
 		async function () {
 			//AQUI EMPIEZA LA PRUEBA
 			//aqui se puede poner trycatch para coger los errores de los awaits
 
-			await arduino.insert_arduino_value(
+			await login.login_data_from_post(
 				 
                 {"body":{
-                    "medida":777
+						"username": "eustaquio",
+					   "password": "putin"
+				 
                 }}
                 
                 )
-			var res = await arduino.obtener_medicion_valor(
-                {"body":{
-                    "medida":777
-                }}
-            )
+
 			//EL ASSERT ES LO QUE VERIFICA SI ESTÁ BIEN O MAL (el 1º parámetro es el que nos da, el 2º parámetro es el que nos tiene que dar, el 3º parámetro es el mensaje de error)
 			//assert.equal(res.length, 1, "¿no hay un resulado?")  // -->  (a, b, c) -> if (a!=b) --> return c 
 			
@@ -41,17 +41,22 @@ describe("Test 1: insertar una Medicion", function () {	//las pruebas se hacen d
 			console.log(res);
 	}) 
 
-	it("Borro las mediciones añadidas en la BBDD",	//Esta prueba es importante porque usa assert
+	it("Modifico información de un usuario de prueba y lo recibo",	//Esta prueba es importante porque usa assert
 	async function () {
 		//AQUI EMPIEZA LA PRUEBA
 		//aqui se puede poner trycatch para coger los errores de los awaits
 
-		await arduino.eliminar_medicion_valor(               
+		var res = await user.update_user(               
 			{"body":{
-				"medida":777
+				"usuario": "eustaquio",
+				"nombre": "aser2je",
+				"mail": "update@gmail.com",
+				"telefono": "884888888"
 			}}
 		
 		);
+
+		console.log(res);
 		
 })
 	
