b6cb9c1651a276b74027513702de871649c2dec7
Pablo <74419051+MrChova20@users.noreply.github.com>
2022-11-23 19:38:08 +0100

    LANDING MAQUETADA

---
+++ frontend/src/components/landing.component.js
@@ -0,0 +1,116 @@
+
+import React from 'react';
+import {  Link } from 'react-router-dom';
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import UselocalStorage from "../hooks/localStorage.hook"
+import contra  from '../assets/img/contra.png'
+import '../assets/files/css/bootstrap-flex.css'
+import '../assets/files/css/style.css'
+
+
+/*
+import '../assets/files/js/bootstrap.min.js'
+import '../assets/files/js/smooth-scroll.min.js'
+*/
+
+
+
+class LandingC extends React.Component{
+
+    constructor(props){
+        super(props);
+     
+    }
+
+     
+
+    render(){
+
+        
+        return(
+            <div>
+
+
+<div>
+        {/* Required meta tags always come first */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <title>SmoothScroll</title>
+        {/* Bootstrap CSS */}
+        <link rel="stylesheet" href="css/bootstrap-flex.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        {/* Scroll */}
+        {/* MENÚ */}
+    
+        {/* FIN MENU */}
+        {/* SECCIÓN 1 */}
+        <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-1 flex-items-xs-middle" id="sec-1">
+            <div className="row">
+              <div className="col-xs-12"><h1>Elije donde  respirar  con Pollution tracker</h1></div>
+              
+            </div>
+          </div>
+        </div>
+        {/* FIN SECCIÓN */}
+        {/* SECCIÓN 2 */}
+        <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-2 flex-items-xs-middle" id="sec-2">
+            <div className="row">
+              
+            </div>
+          </div>
+        </div>
+        {/* FIN SECCIÓN */}
+        {/* SECCIÓN 3 */}
+        <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-3 flex-items-xs-middle" id="sec-3">
+            <div className="row">
+   
+            </div>
+          </div>
+        </div>
+        {/* FIN SECCIÓN */}
+        {/* SECCIÓN 4 */}
+        <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-4 flex-items-xs-middle" id="sec-4">
+            <div className="row">
+          
+            </div>
+          </div>
+        </div>
+        {/* FIN SECCIÓN */}
+
+
+         {/* SECCIÓN 5 */}
+         <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-5 flex-items-xs-middle" id="sec-5">
+            <div className="row">
+          
+            </div>
+          </div>
+        </div>
+        {/* FIN SECCIÓN */}
+
+   {/* SECCIÓN 6 */}
+   <div className="container-fluid">
+          <div className="row flex-items-xs-center slider-6 flex-items-xs-middle" id="sec-6">
+            <div className="row">
+          
+            </div>
+          </div>
+        </div>
+
+        {/* FOOTER */}
+     
+        {/* FIN FOOTER */}
+        {/* jQuery first, then Tether, then Bootstrap JS. */}
+      </div>
+            </div>
+        )
+    }
+}
+
+
+export default LandingC
--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import AreaUSuario from '../components/area_usuario.component'
-import {Home,Header,TestC, Login, Profile, AdminPageC, AdminPage2C, UserRegisterC, UserEditC} from '../components/index'
+import {Home,Header,TestC, Login, Profile, AdminPageC, AdminPage2C, UserRegisterC, UserEditC,LandingC} from '../components/index'
 
 
 const BaseRouter = () => {
@@ -17,7 +17,7 @@ const BaseRouter = () => {
             <Route exact path="/admin_page2" component={AdminPage2C}/>
             <Route exact path="/user_register" component={UserRegisterC}/>
             <Route exact path="/user_edit" component={UserEditC}/>
-
+            <Route exact path="/landing" component={LandingC}/>
 
             
 
