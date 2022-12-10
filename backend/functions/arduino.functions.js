713bfaf89023c653b357fa506fb714bb00f5c07b
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2022-12-10 17:26:28 +0100

    esto enviar valores desde postman

--- backend/functions/arduino.functions.js
+++ backend/functions/arduino.functions.js
@@ -10,7 +10,7 @@ exports.add_measure_db_call = async (data) =>{
     var query = mysql.query("Insert into db.mediciones (idSensor, Valor, Fecha, Latitud, Longitud) VALUES('"+data.idsensor+"','"+data.valor+"','"+data.fecha+"','"+data.latitud+"','"+data.longitud+"')").then((data, error) => {
         
         if (data) {
-            return data
+            return true
         } else {
             return false 
         }
